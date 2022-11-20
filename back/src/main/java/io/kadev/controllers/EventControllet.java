8dc6e7cb8a2a310f39c2d0e8384e411308553569
yassineRh99 <rhazi.yassine99@gmail.com>
2022-11-20 22:15:19 +0100

    Add event service and controller

---
+++ back/src/main/java/io/kadev/controllers/EventControllet.java
@@ -0,0 +1,41 @@
+package io.kadev.controllers;
+
+import Dto.EventDto;
+import io.kadev.models.Event;
+import io.kadev.models.User;
+import io.kadev.services.EventService;
+import io.kadev.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/event")
+public class EventControllet {
+    @Autowired
+    EventService eventService;
+
+    @PostMapping("/add-event")
+    public ResponseEntity<Event> addEvent(@RequestParam EventDto eventDto){
+        Event event = new Event();
+        event.setDescription(eventDto.getDescription());
+        event.setTitle(eventDto.getTitle());
+        event.setAddress_of_location(eventDto.getAddress_of_location());
+        return ResponseEntity.ok().body(eventService.addEvent(event));
+    }
+
+    @GetMapping("/events")
+    public List<Event> getAllEvents(){
+        return eventService.getAllEvent();
+    }
+
+    @GetMapping("/events/{id}")
+    public Event getEventById(@PathVariable int id){
+        return eventService.getEvent(id);
+    }
+
+
+
+}
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     AppRoutingModule,
     AuthModule,
-    CoreModule
+    CoreModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
